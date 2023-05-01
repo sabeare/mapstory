d2c42591806daf9bce21bb081ab475d8617050fa
Niene <niene.boeijen@gmail.com>
2023-05-01 13:58:45 +0200

    restructure code, thinglinks option added to config

---
+++ main.js
@@ -0,0 +1,372 @@
+const initLoad = true;
+const layerTypes = {
+    'fill': ['fill-opacity'],
+    'line': ['line-opacity'],
+    'circle': ['circle-opacity', 'circle-stroke-opacity'],
+    'symbol': ['icon-opacity', 'text-opacity'],
+    'raster': ['raster-opacity'],
+    'fill-extrusion': ['fill-extrusion-opacity'],
+    'heatmap': ['heatmap-opacity']
+}
+
+const alignments = {
+    'left': 'lefty',
+    'center': 'centered',
+    'right': 'righty',
+    'full': 'fully'
+}
+
+function getLayerPaintType(layer) {
+    const layerType = map.getLayer(layer).type;
+    return layerTypes[layerType];
+}
+
+function setLayerOpacity(layer) {
+    const paintProps = getLayerPaintType(layer.layer);
+    paintProps.forEach(function (prop) {
+        const options = {};
+        if (layer.duration) {
+            const transitionProp = prop + "-transition";
+            options = { "duration": layer.duration };
+            map.setPaintProperty(layer.layer, transitionProp, options);
+        }
+        map.setPaintProperty(layer.layer, prop, layer.opacity, options);
+    });
+}
+
+
+//  Pagina opening aanmaken aan hand van config bestand  - Kan natuurlijk ook zelf zonder config. 
+
+const story = document.getElementById('story');
+const features = document.createElement('div');
+features.setAttribute('id', 'features');
+
+const header = document.createElement('div');
+
+if (config.title) {
+    const titleText = document.createElement('h1');
+    titleText.innerText = config.title;
+    header.appendChild(titleText);
+}
+
+if (config.subtitle) {
+    const subtitleText = document.createElement('h2');
+    subtitleText.innerText = config.subtitle;
+    header.appendChild(subtitleText);
+}
+
+if (config.byline) {
+    const bylineText = document.createElement('p');
+    bylineText.innerText = config.byline;
+    header.appendChild(bylineText);
+}
+
+if (header.innerText.length > 0) {
+    header.classList.add(config.theme);
+    header.setAttribute('id', 'header');
+    story.appendChild(header);
+}
+
+
+// Hoofdstukken genereren aan hand van config. 
+
+config.chapters.forEach((record, idx) => {
+    const container = document.createElement('div');
+    const chapter = document.createElement('div');
+
+    if (record.title) {
+        const title = document.createElement('h3');
+        title.innerText = record.title;
+        chapter.appendChild(title);
+    }
+
+    if (record.image) {
+        const image = new Image();
+        image.src = record.image;
+        chapter.appendChild(image);
+    }
+
+    if (record.description) {
+        const story = document.createElement('p');
+        story.innerHTML = record.description;
+        chapter.appendChild(story);
+    }
+
+    if (record.thinglink){
+        const thinglink = document.createElement('iframe');
+        thinglink.src = record.thinglink;
+        thinglink.height = "500"
+        thinglink.width="100%"
+        chapter.appendChild(thinglink)
+    }
+
+    container.setAttribute('id', record.id);
+    container.classList.add('step');
+    if (idx === 0) {
+        container.classList.add('active');
+    }
+
+    chapter.classList.add(config.theme);
+    container.appendChild(chapter);
+    container.classList.add(alignments[record.alignment] || 'centered');
+    if (record.hidden) {
+        container.classList.add('hidden');
+    }
+    features.appendChild(container);
+});
+
+story.appendChild(features);
+
+
+// Footer creëren 
+
+const footer = document.createElement('div');
+
+if (config.footer) {
+    const footerText = document.createElement('p');
+    footerText.innerHTML = config.footer;
+    footer.appendChild(footerText);
+}
+
+if (footer.innerText.length > 0) {
+    footer.classList.add(config.theme);
+    footer.setAttribute('id', 'footer');
+    story.appendChild(footer);
+}
+
+
+
+// MAP background 
+
+mapboxgl.accessToken = config.accessToken;
+
+const transformRequest = (url) => {
+    const hasQuery = url.indexOf("?") !== -1;
+    const suffix = hasQuery ? "&pluginName=scrollytellingV2" : "?pluginName=scrollytellingV2";
+    return {
+        url: url + suffix
+    }
+}
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: config.style,
+    center: config.chapters[0].location.center,
+    zoom: config.chapters[0].location.zoom,
+    bearing: config.chapters[0].location.bearing,
+    pitch: config.chapters[0].location.pitch,
+    interactive: false,
+    transformRequest: transformRequest,
+    projection: config.projection
+});
+
+// Create a inset map if enabled in config.js
+if (config.inset) {
+    const insetMap = new mapboxgl.Map({
+        container: 'mapInset', // container id
+        style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
+        center: config.chapters[0].location.center,
+        // Hardcode above center value if you want insetMap to be static.
+        zoom: 3, // starting zoom
+        hash: false,
+        interactive: false,
+        attributionControl: false,
+        //Future: Once official mapbox-gl-js has globe view enabled,
+        //insetmap can be a globe with the following parameter.
+        //projection: 'globe'
+    });
+}
+
+if (config.showMarkers) {
+    const marker = new mapboxgl.Marker({ color: config.markerColor });
+    marker.setLngLat(config.chapters[0].location.center).addTo(map);
+}
+
+
+// SCROLLAMA 
+
+// instantiate the scrollama
+const scroller = scrollama();
+
+map.on("load", function () {
+    if (config.use3dTerrain) {
+        map.addSource('mapbox-dem', {
+            'type': 'raster-dem',
+            'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+            'tileSize': 512,
+            'maxzoom': 14
+        });
+        // add the DEM source as a terrain layer with exaggerated height
+        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+
+        // add a sky layer that will show when the map is highly pitched
+        map.addLayer({
+            'id': 'sky',
+            'type': 'sky',
+            'paint': {
+                'sky-type': 'atmosphere',
+                'sky-atmosphere-sun': [0.0, 0.0],
+                'sky-atmosphere-sun-intensity': 15
+            }
+        });
+    };
+
+    // As the map moves, grab and update bounds in inset map.
+    if (config.inset) {
+        map.on('move', getInsetBounds);
+    }
+    // setup the instance, pass callback functions
+    scroller
+        .setup({
+            step: '.step',
+            offset: 0.5,
+            progress: true
+        })
+        .onStepEnter(async response => {
+            const current_chapter = config.chapters.findIndex(chap => chap.id === response.element.id);
+            const chapter = config.chapters[current_chapter];
+
+            response.element.classList.add('active');
+            map[chapter.mapAnimation || 'flyTo'](chapter.location);
+
+            // Setting url hash parameters from chapter id 
+            const url = new URL(location);
+            url.hash = chapter.id;
+            history.pushState({}, "", url);
+
+            // Incase you do not want to have a dynamic inset map,
+            // rather want to keep it a static view but still change the
+            // bbox as main map move: comment out the below if section.
+            if (config.inset) {
+                if (chapter.location.zoom < 5) {
+                    insetMap.flyTo({ center: chapter.location.center, zoom: 0 });
+                }
+                else {
+                    insetMap.flyTo({ center: chapter.location.center, zoom: 3 });
+                }
+            }
+            if (config.showMarkers) {
+                marker.setLngLat(chapter.location.center);
+            }
+            if (chapter.onChapterEnter.length > 0) {
+                chapter.onChapterEnter.forEach(setLayerOpacity);
+            }
+            if (chapter.callback) {
+                window[chapter.callback]();
+            }
+            if (chapter.rotateAnimation) {
+                map.once('moveend', () => {
+                    const rotateNumber = map.getBearing();
+                    map.rotateTo(rotateNumber + 180, {
+                        duration: 30000, easing: function (t) {
+                            return t;
+                        }
+                    });
+                });
+            }
+            if (config.auto) {
+                const next_chapter = (current_chapter + 1) % config.chapters.length;
+                map.once('moveend', () => {
+                    document.querySelectorAll('[data-scrollama-index="' + next_chapter.toString() + '"]')[0].scrollIntoView();
+                });
+            }
+        })
+        .onStepExit(response => {
+            const chapter = config.chapters.find(chap => chap.id === response.element.id);
+            response.element.classList.remove('active');
+            if (chapter.onChapterExit.length > 0) {
+                chapter.onChapterExit.forEach(setLayerOpacity);
+            }
+        });
+
+
+    if (config.auto) {
+        document.querySelectorAll('[data-scrollama-index="0"]')[0].scrollIntoView();
+    }
+});
+
+// Helper functions for insetmap
+function getInsetBounds() {
+    let bounds = map.getBounds();
+
+    let boundsJson = {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            bounds._sw.lng,
+                            bounds._sw.lat
+                        ],
+                        [
+                            bounds._ne.lng,
+                            bounds._sw.lat
+                        ],
+                        [
+                            bounds._ne.lng,
+                            bounds._ne.lat
+                        ],
+                        [
+                            bounds._sw.lng,
+                            bounds._ne.lat
+                        ],
+                        [
+                            bounds._sw.lng,
+                            bounds._sw.lat
+                        ]
+                    ]
+                ]
+            }
+        }]
+    }
+
+    if (initLoad) {
+        addInsetLayer(boundsJson);
+        initLoad = false;
+    } else {
+        updateInsetLayer(boundsJson);
+    }
+
+}
+
+function addInsetLayer(bounds) {
+    insetMap.addSource('boundsSource', {
+        'type': 'geojson',
+        'data': bounds
+    });
+
+    insetMap.addLayer({
+        'id': 'boundsLayer',
+        'type': 'fill',
+        'source': 'boundsSource', // reference the data source
+        'layout': {},
+        'paint': {
+            'fill-color': '#fff', // blue color fill
+            'fill-opacity': 0.2
+        }
+    });
+    // // Add a black outline around the polygon.
+    insetMap.addLayer({
+        'id': 'outlineLayer',
+        'type': 'line',
+        'source': 'boundsSource',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': 1
+        }
+    });
+}
+
+function updateInsetLayer(bounds) {
+    insetMap.getSource('boundsSource').setData(bounds);
+}
+
+
+
+// setup resize event
+window.addEventListener('resize', scroller.resize);
